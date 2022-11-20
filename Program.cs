0e07d82038a29e4c4669a1a77b0b834e810ff5b1
chenaoh <jfarova112333@gmail.com>
2022-11-20 17:39:30 -0500

    Leer tres números y escribir el mayor de los tres

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1,n2,n3;
+        Console.WriteLine("intrudusca 3 numeros: ");
+        n1= int.Parse(Console.ReadLine());
+        Console.WriteLine("intrudusca 3 numeros: ");
+        n2= int.Parse(Console.ReadLine());
+        Console.WriteLine("intrudusca 3 numeros: ");
+        n3= int.Parse(Console.ReadLine());
+             if(n1>n2 && n1>n3){
+                
+                Console.WriteLine("el numero mayor es: " +n1);
+            
+            
+            }
+            else if(n2>n1 && n2>n3){
+                
+                Console.WriteLine("el numero mayor es: " +n2);
+            
+            
+            }
+           else  if(n3>n1 && n3>n2){
+                
+                Console.WriteLine("el numero mayor es: " +n3);
+            
+            
+            }
+
+           
+        }
+    }
+
